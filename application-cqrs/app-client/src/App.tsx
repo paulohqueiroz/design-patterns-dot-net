d71074bfef149f60310c7682dae443d52d3aa2fb
Paulo Queiroz <phqs.phqs@gmail.com>
2020-04-07 17:24:32 -0300

    add changes

--- application-cqrs/app-client/src/App.tsx
+++ application-cqrs/app-client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>welcome to my new application</code>
+          <code>tesing my code to my new application</code>
         </p>
         <a
           className="App-link"
