7fc6143533f052bcd2db5666de48f533074d57a6
Paulo Queiroz <phqs.phqs@gmail.com>
2020-04-10 21:54:35 -0300

    add cors rule to api

--- application-cqrs/app/API/Startup.cs
+++ application-cqrs/app/API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors( opt => 
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                })
+            );
+
             services.AddControllers();
         }
 
@@ -43,7 +50,7 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
