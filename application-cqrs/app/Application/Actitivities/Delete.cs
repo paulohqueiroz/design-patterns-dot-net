bf8c8473786b3efdbcb53c32adca6602dc483450
Paulo Queiroz <phqs.phqs@gmail.com>
2020-04-14 14:51:55 -0300

    add delete/update endpoints

---
+++ application-cqrs/app/Application/Actitivities/Delete.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Persitence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Actitivities
+{
+    public class Delete
+    {
+
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if(activity == null)
+                {
+                    throw new Exception("Could not be found");
+                }
+
+                _context.Remove(activity);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("Problem deleting...");
+            }
+        }
+
+
+    }
+}
--- application-cqrs/app/Application/Actitivities/Edit.cs
+++ application-cqrs/app/Application/Actitivities/Edit.cs
@@ -1,9 +1,6 @@
-﻿using Domain;
-using MediatR;
+﻿using MediatR;
 using Persitence;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
